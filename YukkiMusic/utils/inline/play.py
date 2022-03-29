907e23d22927dd140e2f8ce63e3c74c538fa79ff
Coba7878 <101111084+Coba7878@users.noreply.github.com>
2022-03-29 10:31:00 +0700

    Update play.py

--- YukkiMusic/utils/inline/play.py
+++ YukkiMusic/utils/inline/play.py
@@ -38,8 +38,6 @@ def stream_markup(_, videoid):
             InlineKeyboardButton(
                 text=_["PL_B_3"], switch_inline_query_current_chat=""
             ),
-        ],
-        [
             InlineKeyboardButton(
                 text=_["CLOSE_BUTTON"], callback_data="close"
             )
